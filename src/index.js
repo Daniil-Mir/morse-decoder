210c4b37987db7b5e952d1ba1ac7b2f5b5c21f7e
Daniil-Mir <MironovDanill.S.19@gmail.com>
2020-10-06 10:49:13 +0500

    feat: add code

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let temp = '';
+    let str = '';
+    let arr = [];
+    let a = '';
+
+    for (let i = 0; i < expr.length; i++) { // Separating an input string into separate characters
+      if (i > 0 && i % 10 === 0) {
+        arr.push(temp);
+        temp = '';
+      };
+        temp += expr[i];
+      
+      if (i === expr.length - 1) {
+        arr.push(temp);
+        temp = '';
+      }
+    }
+
+    for (let j = 0; j < arr.length; j++) {
+      a = arr[j];
+      for (let i = 0; i < 10; i += 2) { // Decode
+        if (a[i] === '1' && a[i + 1] === '0') { // If a couple of symbols is '10'
+          temp += '.';
+        } else if (a[i] === '1' && a[i + 1] === '1') { // If a couple of symbols is '11'
+          temp += '-';
+        } else if (a[i] === '*' && a[i + 1] === '*') { // If a couple of symbols is '**'
+          temp += '**';
+        }
+      };
+      if (temp === '**********') {
+        str += ' ';
+        temp = '';
+      } else {
+        str += MORSE_TABLE[temp];
+        temp = '';
+      };    
+    };
+    return str;
 }
 
 module.exports = {
